9d2fb910bdcd0874dc97c8111c8d9705c7073c3f
Heron Eto <hhqe@hotmail.com>
2024-01-31 10:28:13 -0300

    first commit

---
+++ src/orders/dto/create-order.dto.ts
@@ -0,0 +1,35 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayNotEmpty,
+  IsInt,
+  IsNotEmpty,
+  IsPositive,
+  IsString,
+  MaxLength,
+  ValidateNested,
+  isNotEmpty,
+} from 'class-validator';
+
+export class CreateOrderDto {
+  @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
+
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  card_hash: string;
+}
+
+export class OrderItemDto {
+  @IsPositive()
+  @IsInt()
+  @IsNotEmpty()
+  quantity: number;
+
+  @MaxLength(36)
+  @IsString()
+  @IsNotEmpty()
+  product_id: string;
+}
---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,19 @@
+import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  price: number;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column()
+  image_url: string;
+}
