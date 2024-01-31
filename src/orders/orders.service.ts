66b80249d195f5c96ff0bf369c40d9c12c4d62b4
Heron Eto <hhqe@hotmail.com>
2024-01-31 11:02:50 -0300

    add auth module

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -12,7 +12,7 @@ export class OrdersService {
     @InjectRepository(Product) private productRepo: Repository<Product>,
   ) {}
 
-  async create(createOrderDto: CreateOrderDto) {
+  async create(createOrderDto: CreateOrderDto & { client_id: number }) {
     const productIds = createOrderDto.items.map((item) => item.product_id);
     const uniqueProductIds = [...new Set(productIds)];
     const products = await this.productRepo.findBy({
@@ -25,7 +25,7 @@ export class OrdersService {
       );
     }
     const order = Order.create({
-      client_id: 1,
+      client_id: createOrderDto.client_id,
       items: createOrderDto.items.map((item) => {
         const product = products.find(
           (product) => product.id === item.product_id,
@@ -41,11 +41,23 @@ export class OrdersService {
     return order;
   }
 
-  findAll() {
-    return this.orderRepo.find();
+  findAll(client_id: number) {
+    return this.orderRepo.find({
+      where: {
+        client_id,
+      },
+      order: {
+        created_at: 'DESC',
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  findOne(id: string, client_id: number) {
+    return this.orderRepo.findOneOrFail({
+      where: {
+        id,
+        client_id,
+      },
+    });
   }
 }
